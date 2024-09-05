569e4e0c21103f9d283b62031e5ab9dacc6b6db9
Sciabarrasi <gianluca_sciabarrasi@hotmail.com>
2024-09-05 19:37:31 -0300

    Twilio messaging by whatsapp

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const client = twilio(accountSid, authToken);
 client.messages
     .create({
         body: 'Your appointment is coming up on July 21 at 3PM',
-        from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+5493404633790'
+        from: process.env.twilioNumber,
+        to: process.env.toNumber
     })
     .then(message => console.log(message.sid))
